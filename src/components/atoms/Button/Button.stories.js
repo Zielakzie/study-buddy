4672d649992e329f3c6957c1506841414c6ea848
Przemek <przemek.zielinski01@gmail.com>
2021-12-13 20:31:56 +0100

    Storybook

---
+++ src/components/atoms/Button/Button.stories.js
@@ -0,0 +1,18 @@
+import { Button } from './Button';
+
+export default {
+  title: 'Components/Atoms/Button',
+  component: Button,
+}
+
+const Template = (args) => <Button {...args}>Read More</Button>;
+
+export const Default = Template.bind({});
+Default.args = {
+  isBig: false,
+}
+
+export const Big = Template.bind({});
+Big.args = {
+  isBig: true,
+}
---
+++ src/components/templates/NewsSection/NewSection.stories.js
@@ -0,0 +1,10 @@
+import NewsSection from './NewsSection';
+
+export default {
+  title: 'Components/templates/NewsSection/NewsSection',
+  component: NewsSection,
+};
+
+const Template = (args) => <NewsSection {...args} />;
+
+export const Default = Template.bind({});
